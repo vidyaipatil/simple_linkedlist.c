befacf83a315c784a3bd2a67ab8cff817fa4c8ff
vidya <vidyaipatil2405@gmail.com>
2024-06-12 13:38:51 +0530

    Adding the comments in program

--- linked_list.c
+++ linked_list.c
@@ -1,63 +1,79 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+//creating the structure for node
 struct node{
 	int data;
 	struct node*next;
 };
+//defining the function for insertion at head side
 struct node* insert_front(struct node*head,int d){
-	struct node*temp=(struct node*)malloc(sizeof(struct node));
-	temp->data=d;
-	temp->next=0;
-	if(head==0){
+
+	struct node*temp=(struct node*)malloc(sizeof(struct node)); //allocating the dynamic memory 
+	temp->data=d; // setting the data part 
+	temp->next=0; //here 0 is considered as NULL
+	if(head==0){  // if LL is empty
 		head=temp;
 		return head;
 		
 	}
-	else{
+	else{     // If atleast one node present in LL
 		temp->next=head;
 		head=temp;
 		return head;
 	}
 }
+//defining the function for deletion of node from front
 struct node* delete_front(struct node*head){
+
+    if(head==0){    //if LL is empty
+		printf("nothing for deletion\n");
+		return ;
+	}
+    else{
+
 	struct node*temp=head;
 	head=head->next;
-	free(temp);
+	free(temp); // realese the acquired memory 
 	return head;
+    }
 }
+// defining the function for insertion of node at tail
 struct node* insert_end(struct node*head,int d){
+
 	struct node*temp=(struct node*)malloc(sizeof(struct node));
 	struct node*n;
 	temp->data=d;
 	temp->next=0;
-	if(head==0){
+	if(head==0){ // if LL is empty
 		head=temp;
 		return head;
 	}
 	n=head;
-	while(n->next!=0){
+	while(n->next!=0){  //Travels the LL until we get last node
 		n=n->next;
 	}
 	n->next=temp;
 	return head;
 }
+// defining the function for deletion of node from the End
 void delete_end(struct node*head){
 	struct node*temp;
-	if(head==0){
-		printf("There is nothing for deletion\n");
+	if(head==0){  //If LL is empty
+		printf("nothing for deletion\n");
 		return ;
 	}
 	else{
-	while(head->next->next!=0){
+	while(head->next->next!=0){  // Travels untill the Second last node of LL
 		head=head->next;
 	}
-	temp=head->next;
+	temp=head->next;  //Set the temp at last node
 	 head->next=0;
-	 free(temp);
+	 free(temp);  // Realese the memory
 		
 	}
 }
+// Defining the function for insertion at the given position 
 void insert_pos(struct node**head,int data,int p){
 	int i;
 	if(p==1){
@@ -68,12 +84,13 @@ void insert_pos(struct node**head,int data,int p){
 	temp->data=data;
 	struct node*n;
 	n=*head;
-	for(i=1;i<p-1;i++){
+	for(i=1;i<p-1;i++){ //Travel untill one node before the given position
 		n=n->next;
 	}
 	temp->next=n->next;
 	n->next=temp;
 }
+//Defining the function for deletion of node at given position
 void delete_pos(struct node**head,int p){
 	int i=0;
 	if(p==1){
@@ -81,23 +98,26 @@ void delete_pos(struct node**head,int p){
 	}
 	struct node*temp;
 	struct node*n;
-	for(i=1;i<p-1;i++){
+	for(i=1;i<p-1;i++){  //Travel untill one node before the given position
 		n=n->next;
 	}
 	temp=n->next;
 	n->next=temp->next;
-	free(temp);
+	free(temp);  //Release the memory
 }
+// Defining the function for printing the LL
 void display(struct node*head){
 	if(head==0){
 		printf("Linked list is empty\n");
 		return ;
 	}
 	else{
-		while(head!=0){
+        printf("\n---------------------------Linked List is---------------------------------\n");
+		while(head!=0){  //travels the whole LL
 			printf("The element is : %d\n",head->data);
 			head=head->next;
 		}
+        printf("\n------------------------------------------------------------------------\n");
 		
 	}
 }
@@ -108,8 +128,10 @@ int main(){
 	head=0;
 	
     do{
+        printf("\n------------------------------------------------------------------------\n");
     	printf("Enter your choice among the following\n");
 	printf("1.insert at first\n2.insert at end\n3.insert at position\n4.delete at front\n5.delete at end\n6.delete at position\n");
+    printf("\n------------------------------------------------------------------------\n");
     scanf("%d",&choice);
     	switch(choice){
     		case 1:{
@@ -161,6 +183,7 @@ int main(){
     	
 	
 	}
+    
 		printf("Do want to repeate it press the key C\n");
     	scanf(" %c",&ch);
 }while(ch=='c' || ch=='C');
